c6900bfc0e590fb64fa19e0ee8aa06fcb952fd41
Colin Smith <cmsgoboston41@gmail.com>
2018-02-12 13:05:20 -0500

    Add netlify-cms initial integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,7 @@ module.exports = {
 				pathToConfigModule: `src/utils/typography`,
 			},
 		},
+		`gatsby-plugin-netlify-cms`,
 		`gatsby-plugin-netlify`,
 	],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import g from "glamorous"
 import Link from "gatsby-link"
+import Script from "react-load-script"
 import Helmet from "react-helmet"
 
 import { rhythm } from "../utils/typography"
@@ -8,27 +9,44 @@ import { rhythm } from "../utils/typography"
 export default ({ data }) => {
 	console.log(data)
 	return (
-  <div>
-    <g.H1 display="inline-block" borderBottom="1px solid">
-				Test Posts
-    </g.H1>
-    <h4>
-      {data.allMarkdownRemark.totalCount} Posts
-    </h4>
-    {data.allMarkdownRemark.edges.map(({ node }) =>
-				(<div key={node.id}>
-  <Link to={node.fields.slug} css={{ textDecoration: `none`, color: `inherit` }}> 
-    <g.H3 marginBottom={rhythm(1 / 4)}>
-      {node.frontmatter.title}{" "}
-      <g.Span color="#BBB"> - {node.frontmatter.date}</g.Span>
-    </g.H3>
-    <p>
-      {node.excerpt}
-    </p>
-  </Link>
-</div>)
-			)}
-  </div>
+	  <div>
+	  	<Script
+          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          onLoad={() => {
+	        if (typeof window !== `undefined` && window.netlifyIdentity) {
+		      window.netlifyIdentity.on("init", user => {
+		        if (!user) {
+		          window.netlifyIdentity.on("login", () => {
+		            document.location.href = "/admin/";
+		          });
+		        }
+		      });
+		    }
+		    window.netlifyIdentity.init();
+          }}
+        />
+        
+	    <g.H1 display="inline-block" borderBottom="1px solid">
+			Test Posts
+	    </g.H1>
+	    <h4>
+	      {data.allMarkdownRemark.totalCount} Posts
+	    </h4>
+	    {data.allMarkdownRemark.edges.map(({ node }) => (
+		    	<div key={node.id}>
+				  <Link to={node.fields.slug} css={{ textDecoration: `none`, color: `inherit` }}> 
+				    <g.H3 marginBottom={rhythm(1 / 4)}>
+				      {node.frontmatter.title}{" "}
+				      <g.Span color="#BBB"> - {node.frontmatter.date}</g.Span>
+				    </g.H3>
+				    <p>
+				      {node.excerpt}
+				    </p>
+				  </Link>
+				</div>
+			)
+		)}
+	  </div>
 	)
 }
 
